0bb5f7734418a9e92ed16eb91cfd61bb01c5183c
Rohit PAI <ropai@nvidia.com>
2022-04-27 16:33:13 +0530

    phosphor-bmc-code-mgmt: add factory reset method to HMC inventory app 	As HMC inventory app replaces the BMC updated app, missing method like factor reset 	need to added to the new HMC inventory app
    Tested:
    Factory reset method works on dbus
    xyz.openbmc_project.Common.FactoryReset interface -          -             -
    .Reset                                  method    -          -             -
    dbus-send --system --print-reply  --dest=xyz.openbmc_project.Software.BMC.Inventory /xyz/openbmc_project/software  xyz.openbmc_project.Common.FactoryReset.Reset
    method return time=1651057062.200255 sender=:1.104 -> destination=:1.139 serial=21 reply_serial=2

--- inventory_manager.hpp
+++ inventory_manager.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include "version_inv_entry.hpp"
-
+#include <xyz/openbmc_project/Common/FactoryReset/server.hpp>
+#include "utils.hpp"
+#include <phosphor-logging/elog.hpp>
 #include <sdbusplus/server.hpp>
 
 #include <filesystem>
@@ -15,12 +17,18 @@ namespace manager
 {
 namespace fs = std::filesystem;
 
+using namespace phosphor::logging;
+
+using InventoryManagerInherit = sdbusplus::server::object::object<
+    sdbusplus::xyz::openbmc_project::Common::server::FactoryReset>;
+
 /**
  * @brief Manages Inventory for BMC firmware versions. This publishes the BMC
  *        version information into a DBUS path for other services to query.
+ *        Handles Factory reset Dbus backend implementation for erasing RWFS 
  *
  */
-class InventoryManager
+class InventoryManager : public InventoryManagerInherit
 {
   public:
     /**
@@ -28,7 +36,8 @@ class InventoryManager
      *
      * @param[in/out] bus dbus service
      */
-    InventoryManager(sdbusplus::bus::bus& bus) : bus(bus)
+    InventoryManager(sdbusplus::bus::bus& bus) :
+        InventoryManagerInherit(bus, SOFTWARE_OBJPATH, false), bus(bus)
     {
         readExistingVersion();
     }
@@ -52,6 +61,21 @@ class InventoryManager
             bus, path, version);
 
         versionPtr->createUpdateableAssociation(SOFTWARE_OBJPATH);
+    }
+    
+    /** @brief BMC factory reset - marks the read-write partition for
+     * recreation upon reboot. */
+    void reset() 
+    {
+        constexpr auto setFactoryResetWait = std::chrono::seconds(3);
+        // Mark the read-write partition for recreation upon reboot.
+        utils::execute("/sbin/fw_setenv", "openbmconce", "factory-reset");
+
+        // Need to wait for env variables to complete, otherwise an immediate reboot
+        // will not factory reset.
+        std::this_thread::sleep_for(setFactoryResetWait);
+
+        log<level::INFO>("BMC factory reset will take effect upon reboot.");            
     }
 
   private:
