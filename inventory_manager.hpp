5c3f81ada7507bccb529c95b48f80544b8de101a
Rohit PAI <ropai@nvidia.com>
2022-08-30 14:32:27 +0530

    nvidia specific complete reset We need to erase the log partition which contains the self-test logs, dumps etc along with the regular rwfs to perform complete reset of the HMC board
    tested:
    busctl call xyz.openbmc_project.Software.BMC.Inventory /xyz/openbmc_project/software com.nvidia.Common.CompleteReset CompleteReset
    
    Shutdown logs >
    Complete reset requested.
    Erasing 64 Kibyte @ c800000 - 100% complete.
    Erasing the data in log partition (/dev/mtd7)
    Clearing read-write overlay filesystem.
    Erasing 64 Kibyte @ 1000000 - 100% complete.
    
    Fixes JIRA https://jirasw.nvidia.com/browse/DGXOPENBMC-2248
    
    Signed-off-by: Rohit PAI <ropai@nvidia.com>

--- inventory_manager.hpp
+++ inventory_manager.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include "version_inv_entry.hpp"
 #include <xyz/openbmc_project/Common/FactoryReset/server.hpp>
+#include <com/nvidia/Common/CompleteReset/server.hpp>
 #include "utils.hpp"
 #include <phosphor-logging/elog.hpp>
 #include <sdbusplus/server.hpp>
@@ -21,14 +22,15 @@ using namespace phosphor::logging;
 
 using InventoryManagerInherit = sdbusplus::server::object::object<
     sdbusplus::xyz::openbmc_project::Common::server::FactoryReset>;
-
+using OemCompleteResetInherit = sdbusplus::server::object::object<
+    sdbusplus::com::nvidia::Common::server::CompleteReset>;
 /**
  * @brief Manages Inventory for BMC firmware versions. This publishes the BMC
  *        version information into a DBUS path for other services to query.
  *        Handles Factory reset Dbus backend implementation for erasing RWFS 
  *
  */
-class InventoryManager : public InventoryManagerInherit
+class InventoryManager : public InventoryManagerInherit, OemCompleteResetInherit
 {
   public:
     /**
@@ -37,7 +39,11 @@ class InventoryManager : public InventoryManagerInherit
      * @param[in/out] bus dbus service
      */
     InventoryManager(sdbusplus::bus::bus& bus) :
-        InventoryManagerInherit(bus, SOFTWARE_OBJPATH, InventoryManagerInherit::action::defer_emit), bus(bus)
+        InventoryManagerInherit(bus, SOFTWARE_OBJPATH,
+                                InventoryManagerInherit::action::defer_emit),
+        OemCompleteResetInherit(bus, SOFTWARE_OBJPATH,
+                                OemCompleteResetInherit::action::defer_emit),
+        bus(bus)
     {
         readExistingVersion();
     }
@@ -77,6 +83,21 @@ class InventoryManager : public InventoryManagerInherit
         std::this_thread::sleep_for(setFactoryResetWait);
 
         log<level::INFO>("BMC factory reset will take effect upon reboot.");            
+    }
+
+    /** @brief BMC complete reset - marks the read-write partition for
+     * recreation upon reboot, clears logs and diagnostic data */
+    void completeReset() 
+    {
+        constexpr auto setCompleteResetWait = std::chrono::seconds(3);
+        // Mark the read-write partition for recreation upon reboot.
+        utils::execute("/sbin/fw_setenv", "openbmconce", "complete-reset");
+
+        // Need to wait for env variables to complete, otherwise an immediate reboot
+        // will not factory reset.
+        std::this_thread::sleep_for(setCompleteResetWait);
+
+        log<level::INFO>("BMC complete reset will take effect upon reboot.");            
     }
 
   private:
